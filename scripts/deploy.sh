d061dd5fbd45471b3403d1d1e6b3bd759caa2851
Joe Karasek <joseph.karasek@gmail.com>
2016-05-19 17:59:04 -0700

    using ssh key for rsync

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@ set -x # Show the output of the following commands (useful for debugging)
 echo -e "Host 159.203.228.62" >> ~/.ssh/config
 echo -e "\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/_site deploy@159.203.228.62:/var/www/html
+rsync -e 'ssh -i ~/.ssh/travis-deploy' -r --delete-after --quiet $TRAVIS_BUILD_DIR/_site deploy@159.203.228.62:/var/www/html
