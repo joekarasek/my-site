503e2f0767a06202cef7674086eae33f2c42da4b
Joe Karasek <joseph.karasek@gmail.com>
2016-05-19 18:28:46 -0700

    changing deploy user

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,5 +4,5 @@ set -x # Show the output of the following commands (useful for debugging)
 echo -e "Host 159.203.228.62" >> ~/.ssh/config
 echo -e "\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 
-scp -i travis-deploy $TRAVIS_BUILD_DIR/_site/ deploy@159.203.228.62:/var/www/html/
+scp -i travis-deploy $TRAVIS_BUILD_DIR/_site/ root@159.203.228.62:/var/www/html/
 # rsync -e 'ssh -i travis-deploy' -r --delete-after --quiet $TRAVIS_BUILD_DIR/_site deploy@159.203.228.62:/var/www/html
