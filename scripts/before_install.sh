1531b7c8ccf13b59c5752b8bf3c0dadfe916a49b
Joe Karasek <joseph.karasek@gmail.com>
2016-05-19 18:11:46 -0700

    tweak travis install

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -5,6 +5,6 @@ set -x # Show the output of the following commands (useful for debugging)
 openssl aes-256-cbc -K $encrypted_22009518e18d_key -iv $encrypted_22009518e18d_iv -in ./travis-deploy.enc -out ./travis-deploy -d
 rm travis-deploy.enc # Don't need it anymore
 chmod 600 travis-deploy
-mv travis-deploy ~/.ssh/id_rsa
+# mv travis-deploy ~/.ssh/id_rsa
 ls ~/.ssh
-cat ~/.ssh/travis-deploy
+# cat ~/.ssh/travis-deploy
